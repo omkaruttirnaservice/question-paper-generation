ddd6c036d46d0992e893c591e9ad26924ad986fb
omkaruttirnaservice <omkaruttirnaservice@gmail.com>
2024-07-11 13:00:18 +0530

    Refactoring

--- web/src/components/Dashboard/Dashboard.jsx
+++ web/src/components/Dashboard/Dashboard.jsx
@@ -1,15 +1,17 @@
 import React from 'react';
+import { FaListUl } from 'react-icons/fa6';
 import { IoCreateOutline } from 'react-icons/io5';
 import { useDispatch } from 'react-redux';
-import { ModalActions } from '../../Store/modal-slice.jsx';
-import { testsSliceActions } from '../../Store/tests-slice.jsx';
+import { useNavigate } from 'react-router-dom';
 import AddTestForm from '../AddTestForm/AddTestForm.jsx';
 import AddTestFormAuto from '../AddTestFormAuto/AddTestFormAuto.jsx';
 import CButton from '../UI/CButton.jsx';
-import { AUTO_TEST, MANUAL_TEST } from '../Utils/Constants.jsx';
+import { ModalActions } from '../../Store/modal-slice.jsx';
+import { testsSliceActions } from '../../Store/tests-slice.jsx';
 
 function Dashboard() {
 	const dispatch = useDispatch();
+	const navigate = useNavigate();
 
 	const createTestHandler = () => {
 		dispatch(ModalActions.toggleModal('create-test-modal'));
@@ -22,8 +24,19 @@ function Dashboard() {
 		dispatch(testsSliceActions.setTestCreationType(AUTO_TEST));
 	};
 
+	const testsListHandler = () => {
+		navigate('/tests-list');
+	};
+
 	return (
 		<div className="container mx-auto mt-6 flex gap-6">
+			<CButton
+				className={'btn--info'}
+				onClick={testsListHandler}
+				icon={<FaListUl />}>
+				Tests List
+			</CButton>
+
 			<CButton onClick={createTestHandler} icon={<IoCreateOutline />}>
 				Create Test (Manual)
 			</CButton>
