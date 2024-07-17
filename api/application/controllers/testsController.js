4fe9bfbecb5ae113372ca3b7c006923dadfa5956
omkaruttirnaservice <omkaruttirnaservice@gmail.com>
2024-07-17 15:50:37 +0530

    Removed console logs

--- api/application/controllers/testsController.js
+++ api/application/controllers/testsController.js
@@ -6,7 +6,6 @@ const testsController = {
 	getList: async (req, res) => {
 		try {
 			let _testsList = await testsModel.getList();
-			console.log(_testsList, '==_testsList==');
 			return sendSuccess(res, _testsList);
 		} catch (error) {
 			return sendError(res, error.message);
@@ -16,7 +15,6 @@ const testsController = {
 	getPublishedList: async (req, res) => {
 		try {
 			let _testsListPublished = await testsModel.getPublishedList();
-			console.log(_testsListPublished, '==_testsList==');
 			return sendSuccess(res, _testsListPublished);
 		} catch (error) {
 			return sendError(res, error.message);
@@ -39,16 +37,13 @@ const testsController = {
 	},
 
 	createTest: async (req, res) => {
-		console.log(req.body, 'createTest');
 		let { test: _t, testQuestions: _q } = req.body;
 		try {
 			let _createTestRes = await testsModel.createTest(_t, _q);
-			console.log(_createTestRes);
 			if (_createTestRes) {
 				return sendSuccess(res, 'Successfully created test');
 			}
 		} catch (error) {
-			console.log(error);
 			return sendError(res, error.message);
 		}
 	},
@@ -65,6 +60,7 @@ const testsController = {
 			}
 
 			let _masterTest = await testsModel.createMasterTest(_t, _top);
+
 			const { id: masterTestId } = _masterTest.toJSON();
 			if (!masterTestId) {
 				throw new Error('Unable to create master test');
@@ -99,10 +95,8 @@ const testsController = {
 					await fetchData(idx + 1);
 				}
 
+				// adding id of selected question to update their selection status later on
 				_randQues.forEach((el) => selectedQueId.push(el.q_id));
-
-				console.log(selectedQueId, '==selectedQueId==');
-				console.log(selectedQueId.length, '==selectedQueId.length==');
 			}
 
 			await fetchData(0);
@@ -116,10 +110,6 @@ const testsController = {
 			// updating the question selection status to 1 to indidate that question is selected
 			let _updateTestQuesSelectionStatusRes =
 				await testsModel.updateTestQueSelectionStatus(selectedQueId);
-			console.log(
-				_updateTestQuesSelectionStatusRes,
-				'_updateTestQuesSelectionStatusRes===='
-			);
 
 			return sendSuccess(res, 'Successfully created auto test');
 		} catch (error) {
@@ -137,7 +127,6 @@ const testsController = {
 			}
 
 			let _checkRes = await testsModel.checkForDuplicateTestKey(testKey);
-			console.log(_checkRes, '==_checkRes==');
 			if (_checkRes.length === 1) {
 				return res.status(200).json({
 					_message: 'Test key already exsists',
@@ -156,16 +145,7 @@ const testsController = {
 
 	publishTest: async (req, res) => {
 		try {
-			// let {
-			// 	test_id_for_publish,
-			// 	batch,
-			// 	publish_date,
-			// 	test_key,
-			// 	test_details: mt,
-			// } = req.body;
-
 			let _publishTestInsert = await testsModel.publishTest(req.body);
-			console.log(_publishTestInsert.toJSON(), '-------');
 			if (_publishTestInsert.toJSON().id) {
 				return sendSuccess(res, {
 					message: 'Successfully published test',
@@ -183,7 +163,6 @@ const testsController = {
 				throw new Error('Invalid test id');
 			}
 			let _publishTestDelete = await testsModel.unpublishTest(id);
-			console.log(_publishTestDelete, '-------');
 			if (_publishTestDelete == 1) {
 				return sendSuccess(res, {
 					message: 'Successfully un-published test',
@@ -209,7 +188,6 @@ const testsController = {
 	updateTestQuestion: async (req, res) => {
 		try {
 			let [_updateRes] = await testsModel.updateTestQuestion(req.body);
-			console.log(_updateRes, '==_updateRes==');
 
 			return sendSuccess(res, 'Successfully updated question');
 		} catch (error) {
@@ -219,32 +197,3 @@ const testsController = {
 };
 
 export default testsController;
-
-// {
-//   test: {
-//     test_name: 'Omkar Test',
-//     test_duration: '10',
-//     marks_per_question: '1',
-//     total_questions: null,
-//     is_negative_marking: '1',
-//     negative_mark: '1',
-//     test_passing_mark: ' 2',
-//     test_creation_type: 'Auto'
-//   },
-//   topicList: [
-//     {
-//       id: 1,
-//       topic_name: 'Topic',
-//       subject_id: 1,
-//       question_count: 2,
-//       selectedCount: '2'
-//     },
-//     {
-//       id: 2,
-//       topic_name: 'topic 2',
-//       subject_id: 2,
-//       question_count: 1,
-//       selectedCount: '1'
-//     }
-//   ]
-// }
