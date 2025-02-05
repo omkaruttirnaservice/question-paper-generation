e29d7b6bacd7e4b6bc2bae840055d2fe9cd9ca05
omkaruttirnaservice <omkaruttirnaservice@gmail.com>
2025-02-05 10:02:41 +0530

    indentation upated

--- api/application/controllers/saveExamsController/saveExamsController.js
+++ api/application/controllers/saveExamsController/saveExamsController.js
@@ -12,7 +12,8 @@ const saveExamsController = {
 		console.log(req.body, '==req.body==');
 		let { student_list, pub_id, exam_paper } = req.body;
 
-		if (student_list.length == 0) throw new ApiError(204, 'No students list found');
+		if (student_list.length == 0)
+			throw new ApiError(204, 'No students list found');
 
 		if (exam_paper.length == 0) throw new ApiError(204, 'No exams list found');
 
@@ -21,30 +22,61 @@ const saveExamsController = {
 		let transact = await sequelize.transaction();
 
 		try {
-			await tm_student_test_list.bulkCreate(student_list, { transaction: transact });
-			await tm_student_question_paper.bulkCreate(exam_paper, { transaction: transact });
+			await tm_student_test_list.bulkCreate(student_list, {
+				transaction: transact,
+			});
+			await tm_student_question_paper.bulkCreate(exam_paper, {
+				transaction: transact,
+			});
 			await transact.commit();
-			return res.status(201).json(new ApiResponse(201, {}, 'Successfully uploaded students and their question paper data'));
+			return res
+				.status(201)
+				.json(
+					new ApiResponse(
+						201,
+						{},
+						'Successfully uploaded students and their question paper data'
+					)
+				);
 		} catch (error) {
 			console.log(error, '==error==');
 			await transact.rollback();
-			throw new ApiError(424, error?.message || 'Something went wrong on server');
+			throw new ApiError(
+				424,
+				error?.message || 'Something went wrong on server'
+			);
 		}
 	}),
 
 	getCandiateExamPaperInfo: asyncHandler(async (req, res) => {
 		console.log(req.query, '==req.query==');
 		let { stud_roll, pub_test_id } = req.query;
-		if (!stud_roll || !pub_test_id) throw new ApiError(400, 'Invalid details passed');
+		if (!stud_roll || !pub_test_id)
+			throw new ApiError(400, 'Invalid details passed');
 
-		let [_studentExamDetails] = await saveExamsModel.getStudentExamReportDetails({ stud_roll, pub_test_id });
+		let [_studentExamDetails] =
+			await saveExamsModel.getStudentExamReportDetails({
+				stud_roll,
+				pub_test_id,
+			});
 
-		let [_studQuestionPaper] = await saveExamsModel.getStudentQuestionPaper({ stud_roll, pub_test_id });
+		let [_studQuestionPaper] = await saveExamsModel.getStudentQuestionPaper({
+			stud_roll,
+			pub_test_id,
+		});
 
-		// console.log(_studentExamDetails, '==_studentExamDetails==');
-		// console.log(_studQuestionPaper, '==_studQuestionPaper==');
+		console.log(_studentExamDetails, '==_studentExamDetails==');
+		console.log(_studQuestionPaper, '==_studQuestionPaper==');
 
-		return res.status(200).json(new ApiResponse(200, { studExam: _studentExamDetails[0], quePaper: _studQuestionPaper }, 'Student exam report full'));
+		return res
+			.status(200)
+			.json(
+				new ApiResponse(
+					200,
+					{ studExam: _studentExamDetails[0], quePaper: _studQuestionPaper },
+					'Student exam report full'
+				)
+			);
 	}),
 };
 export default saveExamsController;
