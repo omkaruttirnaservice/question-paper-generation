79edcace4f7457196a9b52a478e2e2c4f63e8988
omkaruttirnaservice <omkaruttirnaservice@gmail.com>
2025-02-11 12:50:13 +0530

    ref

---
+++ exam panel.md
@@ -0,0 +1,4 @@
+exam panel
+reset session - all table reset - except - aouth
+
+upload attendance - form filling panel
--- web/vite.config.js
+++ web/vite.config.js
@@ -4,15 +4,4 @@ import react from '@vitejs/plugin-react';
 const mode = 'production';
 export default defineConfig({
 	plugins: [react()],
-	server: {
-		proxy: {
-			'/api': {
-				target: mode == 'developement' ? 'http://localhost:3026/' : 'http://64.227.149.129:3025',
-				changeOrigin: true,
-			},
-		},
-		host: 'localhost',
-		open: true,
-		port: 3000,
-	},
 });
